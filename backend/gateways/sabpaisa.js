f2397d2204050926c9de555f6ce526ca6750babf
Shivam Anand <shivam19e@gmail.com>
2026-03-19 14:34:09 +0530

    sabpaisa final

--- backend/gateways/sabpaisa.js
+++ backend/gateways/sabpaisa.js
@@ -25,15 +25,19 @@ const SABPAISA_URL =
 
 function encryptSabPaisa(text) {
 
-  const key = Buffer.from(process.env.SABPAISA_AUTH_KEY.trim(), "utf8").slice(0, 32);
-  const iv = Buffer.from(process.env.SABPAISA_AUTH_IV.trim(), "utf8").slice(0, 16);
+  const key = Buffer.from(process.env.SABPAISA_AUTH_KEY, "utf8").slice(0, 32);
+  const iv = Buffer.from(process.env.SABPAISA_AUTH_IV, "utf8").slice(0, 16);
 
-  const cipher = crypto.createCipheriv("aes-256-cbc", key, iv);
+  const cipher = crypto.createCipheriv(
+    "aes-256-cbc",
+    key,
+    iv
+  );
 
-  let encrypted = cipher.update(text, "utf8", "hex");
-  encrypted += cipher.final("hex");
+  let encrypted = cipher.update(text, "utf8", "base64");
+  encrypted += cipher.final("base64");
 
-  return encrypted.toUpperCase(); // VERY IMPORTANT
+  return encrypted;
 }
 
 /* =====================================================
